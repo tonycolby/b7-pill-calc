aff6d9b912e897173d59156853768f58408b1051
Tony Colby <ancolby@microsoft.com>
2025-03-19 11:23:38 +0000

    Added pill images and values to C and D mount datasets

--- src/components/cMount.js
+++ src/components/cMount.js
@@ -1,38 +1,38 @@
 const cMount = {
   1: {
-    5: { toe: -1, antiSquat: 1, pivotWidth: 0.7, pivotHeight: 0.7, image: './assets/c1mount_1-corner.png'},
-    4: { toe: -1, antiSquat: 0.5, pivotWidth: 0.7, pivotHeight: 0.35, image: './assets/c2mount_1-5-Rcorner.png' },
-    3: { toe: -1, antiSquat: 0, pivotWidth: 0.7, pivotHeight: 0, image: './assets/c3mount_1-center.png' },
-    2: { toe: -1, antiSquat: -0.5, pivotWidth: 0.7, pivotHeight: -0.35, image: './assets/c4mount_1-5-Lcorner.png' },
-    1: { toe: -1, antiSquat: -1, pivotWidth: 0.7, pivotHeight: -0.7, image: './assets/c5mount_1-corner.png' },
+    5: { toe: -1, antiSquat: 1, pivotWidth: 0.7, pivotHeight: 0.7, image: './assets/c1mount_1-corner.png', pill: "./assets/1-corner.png", pillRotation: 0 },
+    4: { toe: -1, antiSquat: 0.5, pivotWidth: 0.7, pivotHeight: 0.35, image: './assets/c2mount_1-5-Rcorner.png', pill: "./assets/1-5-Rcorner.png", pillRotation: 0 },
+    3: { toe: -1, antiSquat: 0, pivotWidth: 0.7, pivotHeight: 0, image: './assets/c3mount_1-center.png', pill: "./assets/1-center.png", pillRotation: 0 },
+    2: { toe: -1, antiSquat: -0.5, pivotWidth: 0.7, pivotHeight: -0.35, image: './assets/c4mount_1-5-Lcorner.png', pill: "./assets/1-5-Lcorner.png", pillRotation: 0 },
+    1: { toe: -1, antiSquat: -1, pivotWidth: 0.7, pivotHeight: -0.7, image: './assets/c5mount_1-corner.png', pill: "./assets/1-corner.png", pillRotation: 90 }
   },
   2: {
-    5: { toe: -0.5, antiSquat: 1, pivotWidth: 0.35, pivotHeight: 0.7, image: './assets/c1mount_1-Lcorner.png' },
-    4: { toe: -0.5, antiSquat: 0.5, pivotWidth: 0.35, pivotHeight: 0.35, image: './assets/c2mount_5-corner.png' },
-    3: { toe: -0.5, antiSquat: 0, pivotWidth: 0.35, pivotHeight: 0, image: './assets/c3mount_5-center.png' },
-    2: { toe: -0.5, antiSquat: -0.5, pivotWidth: 0.35, pivotHeight: -0.35, image: './assets/c4mount_5-corner.png' },
-    1: { toe: -0.5, antiSquat: -1, pivotWidth: 0.35, pivotHeight: -0.7, image: './assets/c5mount_1-5-Rcorner.png' },
+    5: { toe: -0.5, antiSquat: 1, pivotWidth: 0.35, pivotHeight: 0.7, image: './assets/c1mount_1-Lcorner.png', pill: "./assets/1-Lcorner.png", pillRotation: 270 },
+    4: { toe: -0.5, antiSquat: 0.5, pivotWidth: 0.35, pivotHeight: 0.35, image: './assets/c2mount_5-corner.png', pill: "./assets/5-corner.png", pillRotation: 0 },
+    3: { toe: -0.5, antiSquat: 0, pivotWidth: 0.35, pivotHeight: 0, image: './assets/c3mount_5-center.png', pill: "./assets/5-center.png", pillRotation: 0 },
+    2: { toe: -0.5, antiSquat: -0.5, pivotWidth: 0.35, pivotHeight: -0.35, image: './assets/c4mount_5-corner.png', pill: "./assets/5-corner.png", pillRotation: 90 },
+    1: { toe: -0.5, antiSquat: -1, pivotWidth: 0.35, pivotHeight: -0.7, image: './assets/c5mount_1-5-Rcorner.png', pill: "./assets/1-5-Rcorner.png", pillRotation: 90 }
   },
   3: {
-    5: { toe: 0, antiSquat: 1, pivotWidth: 0, pivotHeight: 0.7, image: './assets/c1mount_1-center.png' },
-    4: { toe: 0, antiSquat: 0.5, pivotWidth: 0, pivotHeight: 0.35, image: './assets/c2mount_5-center.png'},
-    3: { toe: 0, antiSquat: 0, pivotWidth: 0, pivotHeight: 0, image: './assets/c3mount_center-center.png' },
-    2: { toe: 0, antiSquat: -0.5, pivotWidth: 0, pivotHeight: -0.35, image: './assets/c4mount_5-center.png' },
-    1: { toe: 0, antiSquat: -1, pivotWidth: 0, pivotHeight: -0.7, image: './assets/c5mount_1-center.png' },
+    5: { toe: 0, antiSquat: 1, pivotWidth: 0, pivotHeight: 0.7, image: './assets/c1mount_1-center.png', pill: "./assets/1-center.png", pillRotation: 270},
+    4: { toe: 0, antiSquat: 0.5, pivotWidth: 0, pivotHeight: 0.35, image: './assets/c2mount_5-center.png', pill: "./assets/5-center.png", pillRotation: 270 },
+    3: { toe: 0, antiSquat: 0, pivotWidth: 0, pivotHeight: 0, image: './assets/c3mount_center-center.png', pill: "./assets/center-center", pillRotation: 0 },
+    2: { toe: 0, antiSquat: -0.5, pivotWidth: 0, pivotHeight: -0.35, image: './assets/c4mount_5-center.png', pill: "./assets/5-center.png", pillRotation: 90 },
+    1: { toe: 0, antiSquat: -1, pivotWidth: 0, pivotHeight: -0.7, image: './assets/c5mount_1-center.png', pill: "./assets/1-center.png", pillRotation: 90 }
   },
   4: {
-    5: { toe: 0.5, antiSquat: 1, pivotWidth: -0.35, pivotHeight: 0.7, image: './assets/c1mount_1-5-Rcorner.png' },
-    4: { toe: 0.5, antiSquat: 0.5, pivotWidth: -0.35, pivotHeight: 0.35, image: './assets/c2mount_5-cornert.png' },
-    3: { toe: 0.5, antiSquat: 0, pivotWidth: -0.35, pivotHeight: 0, image: './assets/c3mount_5-centert.png' },
-    2: { toe: 0.5, antiSquat: -0.5, pivotWidth: -0.35, pivotHeight: -0.35, image: './assets/c4mount_5-cornert.png' },
-    1: { toe: 0.5, antiSquat: -1, pivotWidth: -0.35, pivotHeight: -0.7, image: './assets/c5mount_1-5-Lcorner.png' },
+    5: { toe: 0.5, antiSquat: 1, pivotWidth: -0.35, pivotHeight: 0.7, image: './assets/c1mount_1-5-Rcorner.png', pill: "./assets/1-5-Rcorner.png", pillRotation: 270 },
+    4: { toe: 0.5, antiSquat: 0.5, pivotWidth: -0.35, pivotHeight: 0.35, image: './assets/c2mount_5-cornert.png', pill: "./assets/5-corner.png", pillRotation: 270 },
+    3: { toe: 0.5, antiSquat: 0, pivotWidth: -0.35, pivotHeight: 0, image: './assets/c3mount_5-centert.png', pill: "./assets/5-center.png", pillRotation: 180 },
+    2: { toe: 0.5, antiSquat: -0.5, pivotWidth: -0.35, pivotHeight: -0.35, image: './assets/c4mount_5-cornert.png', pill: "./assets/5-corner.png", pillRotation: 180 },
+    1: { toe: 0.5, antiSquat: -1, pivotWidth: -0.35, pivotHeight: -0.7, image: './assets/c5mount_1-5-Lcorner.png', pill: "./assets/1-5-Lcorner.png", pillRotation: 90 }
   },
   5: {
-    5: { toe: 1, antiSquat: 1, pivotWidth: -0.7, pivotHeight: 0.7, image: './assets/c1mount_1-cornert.png' },
-    4: { toe: 1, antiSquat: 0.5, pivotWidth: -0.7, pivotHeight: 0.35, image: './assets/c2mount_1-5-Rcornert.png' },
-    3: { toe: 1, antiSquat: 0, pivotWidth: -0.7, pivotHeight: 0, image: './assets/c3mount_1-centert.png' },
-    2: { toe: 1, antiSquat: -0.5, pivotWidth: -0.7, pivotHeight: -0.35, image: './assets/c4mount_1-5-Rcornert.png' },
-    1: { toe: 1, antiSquat: -1, pivotWidth: -0.7, pivotHeight: -0.7, image: './assets/c5mount_1-cornert.png' },
+    5: { toe: 1, antiSquat: 1, pivotWidth: -0.7, pivotHeight: 0.7, image: './assets/c1mount_1-cornert.png', pill: "./assets/1-corner.png", pillRotation: 270 },
+    4: { toe: 1, antiSquat: 0.5, pivotWidth: -0.7, pivotHeight: 0.35, image: './assets/c2mount_1-5-Rcornert.png', pill: "./assets/1-5-Rcorner.png", pillRotation: 180 },
+    3: { toe: 1, antiSquat: 0, pivotWidth: -0.7, pivotHeight: 0, image: './assets/c3mount_1-centert.png', pill: "./assets/1-center.png", pillRotation: 180 },
+    2: { toe: 1, antiSquat: -0.5, pivotWidth: -0.7, pivotHeight: -0.35, image: './assets/c4mount_1-5-Rcornert.png', pill: "./assets/1-5-Rcorner.png", pillRotation: 180 },
+    1: { toe: 1, antiSquat: -1, pivotWidth: -0.7, pivotHeight: -0.7, image: './assets/c5mount_1-cornert.png', pill: "./assets/1-corner.png", pillRotation: 180 }
   },
 };
 
